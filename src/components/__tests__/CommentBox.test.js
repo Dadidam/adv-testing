dfe118ce448cb55edbb9e7dad391a3a485b727a7
Iliav <iliav@hurdit.com>
2018-07-19 19:48:40 -0400

    Using describe block to combine tests

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -3,6 +3,7 @@ import { mount, unmount, simulate, update, prop } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
 let wrapped;
+const value = 'new comment';
 
 beforeEach(() => {
   wrapped = mount(<CommentBox />);
@@ -17,25 +18,20 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  const value = 'new comment';
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', { target: { value } });
+    wrapped.update();
+  });
 
-  wrapped.find('textarea').simulate('change', { target: { value } });
-  wrapped.update();
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual(value);
+  });
 
-  expect(wrapped.find('textarea').prop('value')).toEqual(value);
-});
+  it('clears text area after form submission', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
 
-it('clears text area after form submission', () => {
-  const value = 'new comment';
-
-  wrapped.find('textarea').simulate('change', { target: { value } });
-  wrapped.update();
-
-  expect(wrapped.find('textarea').prop('value')).toEqual(value);
-
-  wrapped.find('form').simulate('submit');
-  wrapped.update();
-
-  expect(wrapped.find('textarea').prop('value')).toEqual('');
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
